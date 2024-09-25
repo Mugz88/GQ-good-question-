c8308ad00433344d557f3aba28c47abd636bd30b
Alexndr <mugz_play@rambler.ru>
2024-09-25 22:03:01 +0400

    html+sattings git

--- backend/main/views.py
+++ backend/main/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    context: dict ={   
+        'title': "Главная",
+        'content': "Содержание главной страницы",
+    }
+    return render(request, 'main/index.html', context)
 
 def about(request):
     return HttpResponse("About page.")
