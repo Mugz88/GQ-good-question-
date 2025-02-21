a2c646a4469a578d3f976c3b9962035b6dd05876
Mugz88 <mugz_play@rambler.ru>
2025-02-21 16:29:21 +0400

    update page about

--- backend/main/views.py
+++ backend/main/views.py
@@ -7,7 +7,7 @@ def index(request):
     questions = Questions.objects.all().order_by('-date')
     context: dict ={   
         'title': "Главная",
-        'content': "Содержание главной страницы",
+        'content': "Вопросы на сайте",
         'questions': questions,
     }
     return render(request, 'main/index.html', context)
@@ -15,7 +15,7 @@ def index(request):
 def about(request):
     context: dict ={   
         'title': "О сайте",
-        'content': "Описание сайта",
-        'text_on_page': "Текст на странице",
+        'content': "Курсовая работа ",
+        'text_on_page': "Выполнил: Натальин Александр ИВТ-13",
     }
     return render(request, 'main/about.html', context)
